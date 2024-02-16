a46825ea0580d061cf2ff47aabfc4287ba17decd
sneko <supernekocat31@gmail.com>
2024-02-16 23:41:22 +0900

    add docstring

--- src/imgcode.py
+++ src/imgcode.py
@@ -1,11 +1,27 @@
 import base64
 
 def encode_img(img_path):
+    """画像ファイルをBase64形式でエンコード
+
+    Args:
+        img_path (str): 画像ファイルのパス
+
+    Returns:
+        bytes: Base64形式でエンコードされた画像データ
+    """
     with open(img_path, 'br') as f:
         data = base64.b64encode(f.read())
     return data
 
 def decode_data(data):
+    """Base64形式のデータをデコード
+
+    Args:
+        data (bytes): Base64形式のデータ
+
+    Returns:
+        bytes: デコードされたデータ
+    """
     return base64.b64decode(data)
 
 if __name__ == '__main__':
--- src/main.py
+++ src/main.py
@@ -4,10 +4,26 @@ import requests
 REQUEST_URL = 'https://zipcloud.ibsnet.co.jp/api/search'
 
 def get_zipcloud(zipcode):
+    """郵便番号から住所情報を取得
+
+    Args:
+        zipcode (str): 郵便番号
+
+    Returns:
+        dict: 郵便番号に関連する住所情報を含むJSONレスポンス
+    """
     res = requests.get(REQUEST_URL, params={'zipcode': zipcode})
     return res.json()
 
 def get_address(data):
+    """住所情報から住所を取得
+
+    Args:
+        data (dict): 住所情報を含むJSONデータ
+
+    Returns:
+        str: 住所
+    """
     if data['results'] != None:
         results = data['results'][0]
         return results['address1'] + results['address2'] + results['address3']
